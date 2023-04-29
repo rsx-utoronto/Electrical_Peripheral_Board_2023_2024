b9da31bdb12a30b98cf4b11d264adba3154aa0bc
Davis Ngresi <116968545+Davis-NG@users.noreply.github.com>
2023-04-29 15:27:48 -0400

    RGB lights and serial communications

---
+++ src/code.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials NeoPixel example"""
+import time
+import board
+from rainbowio import colorwheel
+import neopixel
+
+pixel_pin = board.GP0
+control_pin = board.GP1
+num_pixels = 6
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+
+def color_chase(color, wait):
+    for i in range(num_pixels):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.show()
+    time.sleep(0.5)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = colorwheel(rc_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def blink(color, wait):
+    pixels.fill(BLACK)
+    pixels.show()
+    time.sleep(wait)
+    
+    pixels.fill(color)
+    pixels.show()
+    time.sleep(wait)
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+BLACK = (0, 0, 0)
+
+while True:
+    for i in range(5):
+        blink(GREEN, 0.5)
+
+    rainbow_cycle(0)  # Increase the number to slow down the rainbow
