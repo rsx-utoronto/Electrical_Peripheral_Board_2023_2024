7284a53708a978229cc7081f2b6f2dfc29f6490f
Krishanth Suthaharan <73756462+kris20012@users.noreply.github.com>
2023-05-17 21:57:41 -0400

    Uploading working code version of code.py as seen in demo

--- test/Circuitpython/code.py
+++ test/Circuitpython/code.py
@@ -2,8 +2,13 @@ import time
 import board
 from rainbowio import colorwheel
 import neopixel
-import usb_cdc
-import USB
+#import usb_cdc
+#import USB
+import pwmio
+
+RBright = pwmio.PWMOut(board.GP9, frequency = 5000, duty_cycle = 0)
+BBright = pwmio.PWMOut(board.GP10, frequency = 5000, duty_cycle = 0)
+GBright = pwmio.PWMOut(board.GP11, frequency = 5000, duty_cycle = 0)
 
 pixel_pin = board.GP22
 num_strip = 50
@@ -31,6 +36,18 @@ def rainbow_cycle(wait):
         fan.show()
         time.sleep(wait)
 
+def colorFill(color):
+    fan.fill(color)
+    fan.show()
+    strip_colour = (color[0], color[2], color[1])
+    strip.fill(strip_colour)
+    strip.show()
+    bright_fill(color)
+   
+def bright_fill(colour):
+    RBright.duty_cycle = int(65535 * ((colour[0] / 255)))
+    GBright.duty_cycle = int(65535 * ((colour[1] / 255)))
+    BBright.duty_cycle = int(65535 * ((colour[2] / 255)))
 
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
@@ -43,9 +60,9 @@ PURPLE = (180, 0, 255)
 while True:
 #     print(USB.read_data(1))
 #     USB.send_data("Helooo!")
-#     time.sleep(1)
-    fan.fill(RED)
-    fan.show()
+#]     time.sleep(1)
+#    fan.fill(RED)
+#    fan.show()
 #     # Increase or decrease to change the speed of the solid color change.
 #     time.sleep(1)
 #     fan.fill(GREEN)
@@ -54,12 +71,19 @@ while True:
 #     fan.fill(BLUE)
 #     fan.show()
 #     time.sleep(1)
-# 
+#
 #     color_chase(RED, 0.1)  # Increase the number to slow down the color chase
 #     color_chase(YELLOW, 0.1)
 #     color_chase(GREEN, 0.1)
 #     color_chase(CYAN, 0.1)
 #     color_chase(BLUE, 0.1)
 #     color_chase(PURPLE, 0.1)
-# 
+#
 #     rainbow_cycle(0)  # Increase the number to slow down the rainbow
+#
+     colorFill(RED)  # Test the strip
+     time.sleep(1)
+     colorFill(GREEN)
+     time.sleep(1)
+     colorFill(BLUE)
+     time.sleep(1)
