d49472e87b6710c80c60feff8f3213ac027a5a5e
wangjo16 <73364347+wangjo16@users.noreply.github.com>
2023-05-27 14:44:18 -0400

    Add files via upload

---
+++ serial_rx_may27b.ino
@@ -0,0 +1,12 @@
+char Mymessage[1]; //Initialized variable to store recieved data
+  
+  void setup() {
+    // Begin the Serial at 9600 Baud
+    Serial.begin(9600);
+  }
+  
+  void loop() {
+    Serial.readBytes(Mymessage,1); //Read the serial data and store in var
+    Serial.println(Mymessage); //Print data on Serial Monitor
+    delay(1000);
+  }
