51b8b3fd1a22089e20d4e09147c5063db43bb562
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2023-11-11 15:17:30 -0500

    Add files via upload

--- code/master.ino
+++ code/master.ino
@@ -1,17 +1,13 @@
 // Master Arduino Code:
 #include <SPI.h>  //Library for SPI
-#define green 0
-#define red 2
-#define blue 3
+#include <Arduino.h>
+#include "SPIadditional.h"
 
-void SPI_Reset();
-int16_t SPI_Read(byte address, int DataSize);
-byte SPI_ReadRXBuffer(int n, int m);
-void SPI_LoadTXBuffer(int a, int b, int c, byte dataIn);
-void SPI_RTS(int T2, int T1, int T0);
-void SPI_bitModify(byte address, byte mask, byte data);
-byte SPI_readStatus();
-byte SPI_rxStatus();
+#define green 1
+#define blue 2
+#define red 3
+
+
 
 void setup(void) {
   Serial.begin(115200);      // Starts Serial Communication at Baud Rate 115200
@@ -32,9 +28,18 @@ void loop(void) {
   int RED = 4;
   int GREEN = 2;
   byte Mastereceive;
+  byte color;
+  
   
   while (true) {
-    Mastereceive = SPI_ReadRXBuffer(0,0);
+  Mastereceive = SPI_ReadRXBuffer(0,0);
+  Serial.println("Input the LED you want on:");
+  int input = Serial.parseInt();
+  if (input != 0) {
+    Mastereceive = input;
+  }
+  Serial.print(color);
+  
 
     if (Mastereceive == green) {  // Logic for setting the LED output depending
                                   // upon value received from slave
@@ -69,94 +74,4 @@ void loop(void) {
 
     delay(1000);
   } 
-}
-
-void SPI_Reset(){
-  digitalWrite(SS, LOW);
-  byte reset = 0xC0;
-  SPI.transfer(&reset, 1);
-  digitalWrite(SS, HIGH);
-}
-
-int16_t SPI_Read(byte address, int DataSize){ //datasize in bytes <=2
-  digitalWrite(SS, LOW);
-  byte instruction = 3; //b00000011
-  int16_t buffer = (instruction<<8)|(address);
-  SPI.transfer(&buffer, 2);
-  int16_t dataRecieved;
-  if(DataSize == 1){
-    byte dataReceive1 = SPI.transfer(&buffer);
-    dataRecieved = dataReceive1;
-  }
-  else if(DataSize == 2){
-    dataRecieved = SPI.transfer16(&buffer); 
-  }
-  else{
-    Serial.println("SPI_Read DataSize is too big");
-  }
-  digitalWrite(SS, HIGH);
-  return dataRecieved;
-}
-
-byte SPI_ReadRXBuffer(int n, int m){
- byte instruction = 10010000|(n<<1)| m; 
- //read data sheet
- digitalWrite(SS, LOW);
- int16_t buffer = (instruction<<8);
- int16_t recieved= SPI.transfer(&buffer);
- byte dataRecieved = (recieved<<8);
- digitalWrite(SS, HIGH);
- Serial.println(recieved);
- return dataRecieved;
-}
-
-void SPI_Write(byte address, byte data){ //for 1 byte of data
-  digitalWrite(SS, LOW);
-  byte instruction = 2; //b00000010
-  long buffer = (instruction<<24)|(address<<16)|(data<<8);
-  SPI.transfer(&buffer, 3);
-  digitalWrite(SS, HIGH);
-  
-}
-
-void SPI_LoadTXBuffer(int a, int b, int c, byte dataIn){
-  digitalWrite(SS, LOW);
-  byte instruction = 01000000 | (a<<2) | (b<<1) | c;
-  int buffer = (instruction<<8)|(dataIn);
-  SPI.transfer(&buffer, 2);
-  digitalWrite(SS, HIGH);
-}
-
-void SPI_RTS(int T2, int T1, int T0){
-  digitalWrite(SS, LOW);
-  byte buffer = (10000000 | (T2<<2)|(T1<<1)|(T0));
-  digitalWrite(SS, HIGH);
-}
-void SPI_bitModify(byte address, byte mask, byte data){
-  digitalWrite(SS, LOW);
-  byte instruction = 5; //0000101?
-  long buffer;//32 bit (4bytes)
-  buffer = (instruction << 24)|(address << 16)|(mask << 8)|(data);
-  SPI.transfer(&buffer, 4);
-  digitalWrite(SS, HIGH);
-  }
-
-byte SPI_readStatus(){
-  digitalWrite(SS, LOW);
-  byte instruction = 160; //10100000
-  SPI.transfer(&instruction, 1);
-  int16_t received = SPI.transfer16(&instruction); //don't care about sending
-  byte dataOut = received;
-  digitalWrite(SS, HIGH);
-  return dataOut;
-}
-
-byte SPI_rxStatus(){
-  digitalWrite(SS, LOW);
-  byte instruction = 176; //10110000
-  SPI.transfer(&instruction, 1);
-  int16_t received = SPI.transfer16(&instruction); //don't care about sending
-  byte dataOut = received;
-  digitalWrite(SS, HIGH);
-  return dataOut;
 }
